7d3e17ef7b5fa71020d02a9aa7cf94770d37fd19
Jay Boehm <jayrox@gmail.com>
2016-12-30 00:22:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express      = require('express');
 var _            = require('lodash');
+var path         = require('path');
 var readdir      = require('fs-readdir-promise');
 
 var config = require(__dirname + '/lib/config');
@@ -30,7 +31,7 @@ app.get('/:app-:version.bin', function(req, res, next) {
       }
     });
     if (versions.length == 0) {
-      res.send("Firmware not found.");
+      res.status(404).send("Firmware not found.");
       return;
     }
     return versions;
@@ -43,10 +44,17 @@ app.get('/:app-:version.bin', function(req, res, next) {
   })
   .then(function (version) {
     if (req.params.version < version.version) {
-      var fullUrl = req.protocol + '://' + req.get('host') + '/' + version.name;
-      res.send(fullUrl);
+      console.log("Sending binary");
+      var filePath = path.join(__dirname, './public/bin', version.name);
+      console.log(filePath);
+      var options = {
+        headers: {
+          'Content-Disposition': 'attachment; filename=' + version.name
+        }
+      };
+      res.sendFile(filePath, options);
     }else {
-      res.send("No newer firmware available.");
+      res.status(304).send("No newer firmware available.");
     }
   })
 });
